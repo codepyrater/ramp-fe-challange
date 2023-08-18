f5bd77d70548d6f09f4213d9a3ba7c216d7dcd3b
codepyrater <punna1s@cmich.edu>
2023-08-18 14:08:40 -0400

    fixed

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -1,16 +1,26 @@
-import classNames from "classnames"
-import { useRef } from "react"
-import { InputCheckboxComponent } from "./types"
+import React, { useState, useRef } from 'react';
+import classNames from 'classnames';
+import { InputCheckboxComponent } from './types';
 
-export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
-  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked: initialChecked = false, disabled, onChange }) => {
+  const [checked, setChecked] = useState(initialChecked);
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`);
+
+  const handleInputChange = () => {
+    const newChecked = !checked;
+    setChecked(newChecked);
+    if (onChange) {
+      onChange(newChecked);
+    }
+  };
 
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
-        className={classNames("RampInputCheckbox--label", {
-          "RampInputCheckbox--label-checked": checked,
-          "RampInputCheckbox--label-disabled": disabled,
+        htmlFor={inputId}
+        className={classNames('RampInputCheckbox--label', {
+          'RampInputCheckbox--label-checked': checked,
+          'RampInputCheckbox--label-disabled': disabled,
         })}
       />
       <input
@@ -19,8 +29,8 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
         className="RampInputCheckbox--input"
         checked={checked}
         disabled={disabled}
-        onChange={() => onChange(!checked)}
+        onChange={handleInputChange}
       />
     </div>
-  )
-}
+  );
+};
